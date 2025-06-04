5dc54c30001d66ed2d83b5e083e2c2487f2a6a61
heejin <heejin0857@naver.com>
2025-06-05 04:41:53 +0900

    Feat : TanStack Query setting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import "./globals.css";
+import QueryProvider from '@/querys/QueryProvider';
+import './globals.css';
 
 export default function RootLayout({
   children,
@@ -7,7 +8,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <QueryProvider>
+        <body>{children}</body>
+      </QueryProvider>
     </html>
   );
 }
