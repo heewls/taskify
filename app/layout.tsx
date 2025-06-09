cf1037734cf1f85f6bc5609ad71388b576b7de93
heejin <heejin0857@naver.com>
2025-06-10 04:57:01 +0900

    Feat : favicon & metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
+import type { Metadata } from 'next';
 import QueryProvider from '@/querys/QueryProvider';
 import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Taskify',
+};
 
 export default function RootLayout({
   children,
