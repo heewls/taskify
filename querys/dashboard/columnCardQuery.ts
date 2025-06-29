62f371e0cc2d0eba9214aa1ae9ef8bc72dc9b8bb
heewls <heejin0857@naver.com>
2025-06-29 21:15:15 +0900

    Fix : vercel error

---
+++ querys/dashboard/columnCardQuery.ts
@@ -0,0 +1,50 @@
+import { keepPreviousData, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { apiClient } from '@/lib/apiClient';
+import EXTERNAL_API from '@/constants/api/external';
+import { getDndCards } from '@/components/Dashboard/DashboardColumn/getDndCards';
+import { Card, CardPayload } from '@/components/Dashboard/type';
+
+export function useGetColumnCards(columnId: number) {
+  return useQuery({
+    queryKey: ['column-cards', columnId],
+    queryFn: () => getDndCards(columnId),
+    placeholderData: keepPreviousData,
+  });
+}
+
+export function useManageColumnCards({ payload, card }: { payload: CardPayload; card?: Card }) {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: async () => {
+      const url = card ? `${EXTERNAL_API.CARDS.ROOT}/${card.id}` : `${EXTERNAL_API.CARDS.ROOT}`;
+      const method = card ? apiClient.put : apiClient.post;
+
+      return await method(url, payload);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['column-cards', payload.columnId],
+      });
+    },
+  });
+
+  return { ...mutation, manageCardMutation: mutation.mutateAsync };
+}
+
+export function useDeleteColumnCard({ cardId, columnId }: { cardId: number; columnId: number }) {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: async () => {
+      await apiClient.delete(`${EXTERNAL_API.CARDS.ROOT}/${cardId}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['column-cards', columnId],
+      });
+    },
+  });
+
+  return { ...mutation, deleteCardMutation: mutation.mutateAsync };
+}
